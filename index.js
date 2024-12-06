2212c8ccad9030f80bf054bd09d989eba6c7f0d7
Georgi 🧡 <georgina.berrezel@gmail.com>
2024-12-06 11:26:39 +0100

    install express and api local ok

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/api/ping", (req, res) => {
 
 // Initialize server
 app.listen(3000, () => {
-  console.log("Running on port 000.");
+  console.log("Running on port 3000.");
 });
 
 module.exports = app;
